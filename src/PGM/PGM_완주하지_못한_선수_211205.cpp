d38af99a5a022aa03413eca70654c907b112d2aa
Raemin Kang <foals18@gamil.com>
2021-12-05 20:49:30 +0900

    feat : 프로그래머스 완주하지 못한 선수 업로드

---
+++ src/PGM/PGM_완주하지_못한_선수_211205.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    for (int i = 0; i < participant.size(); i++) {
+        if (participant[i] != completion[i]) {
+            answer = participant[i];
+            break;
+        }
+    }
+    return answer;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+//    vector<string> participant{"leo", "kiki", "eden"};
+//    vector<string> participant{"marina", "josipa", "nikola", "vinko", "filipa"};
+    vector<string> participant{"mislav", "stanko", "mislav", "ana"};
+//    vector<string> completion{"eden", "kiki"};
+//    vector<string> completion{"josipa", "filipa", "marina", "nikola"};
+    vector<string> completion{"stanko", "ana", "mislav"};
+    cout << solution(participant, completion) << "\n";
+    return 0;
+}
