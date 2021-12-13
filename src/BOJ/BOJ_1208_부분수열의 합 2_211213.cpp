9b5ec88ad8fb4d3bcb4dd64b2f9b12a949eca9b8
Raemin Kang <foals18@gmail.com>
2021-12-13 17:34:21 +0900

    feat: 백준 1208번 부분수열의 합 2 업로드

---
+++ src/BOJ/BOJ_1208_부분수열의 합 2_211213.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LL;
+int N, S;
+LL answer;
+vector<int> v;
+vector<LL> leftV, rightV, leftSum, rightSum;
+
+void func(const vector<LL> &arr, vector<LL> &target, const int idx, const LL sum) {
+    if (idx == arr.size()) {
+        target.push_back(sum);
+        return;
+    }
+    func(arr, target, idx + 1, sum);
+    func(arr, target, idx + 1, sum + arr[idx]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N >> S;
+    v.assign(N, 0);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+        (i < N / 2 ? leftV : rightV).push_back(v[i]);
+    }
+    func(leftV, leftSum, 0, 0);
+    func(rightV, rightSum, 0, 0);
+    sort(leftSum.begin(), leftSum.end());
+    for (const LL d: rightSum) {
+        answer +=
+                upper_bound(leftSum.begin(), leftSum.end(), S - d) - lower_bound(leftSum.begin(), leftSum.end(), S - d);
+    }
+    cout << (S == 0 ? answer - 1 : answer) << "\n";
+    return 0;
+}
