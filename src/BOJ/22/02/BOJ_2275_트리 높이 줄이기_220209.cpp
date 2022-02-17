46cf075b9f7f10f20ef5a587ee4909e4b4a7a8b7
Raemin Kang <foals18@gmail.com>
2022-02-17 19:19:00 +0900

    feat: 백준 2275번 트리 높이 줄이기 업로드

---
+++ src/BOJ/22/02/BOJ_2275_트리 높이 줄이기_220209.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int N, H, root;
+vector<map<int, int>> nodes;
+vector<int> answer;
+vector<int> dist, mx;
+
+int dfs(const int cur) {
+    mx[cur] = dist[cur];
+    for (const pair<int, int> &d: nodes[cur]) {
+        dist[d.first] = dist[cur] + d.second;
+        mx[cur] = max(mx[cur], dfs(d.first));
+    }
+    return mx[cur];
+}
+
+int solution(const int cur, const int delta) {
+    int result = 0;
+    for (const pair<int, int> &d: nodes[cur]) {
+        if (mx[d.first] - delta <= H) continue;
+        if (mx[d.first] - delta - d.second <= H) result += mx[d.first] - delta - H;
+        else result += solution(d.first, delta + d.second) + d.second;
+    }
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N >> H;
+    nodes.assign(N, map<int, int>());
+    dist.assign(N, 0);
+    mx.assign(N, 0);
+    for (int i = 0; i < N; i++) {
+        int a = 0, b = 0;
+        cin >> a >> b;
+        if (a == 0 && b == 0) {
+            root = i;
+            continue;
+        }
+        a--;
+        nodes[a][i] = b;
+    }
+    dfs(root);
+    cout << solution(root, 0) << "\n";
+    return 0;
+}
