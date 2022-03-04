67037a93d2109339d390db2ab7e2067d6aa7381f
Raemin Kang <foals18@gmail.com>
2022-03-05 07:39:23 +0900

    feat: 백준 1208번 부분수열의 합 2 업로드

---
+++ src/BOJ/22/03/BOJ_1208_부분수열의 합 2_220305.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LL;
+
+int N, S, answer;
+vector<int> leftV, rightV;
+vector<LL> leftSum, rightSum;
+
+void solution(const vector<int> &arr, vector<LL> &sumV, int idx, LL sum) {
+    if (idx == arr.size()) {
+        sumV.push_back(sum);
+        return;
+    }
+    solution(arr, sumV, idx + 1, sum);
+    solution(arr, sumV, idx + 1, sum + arr[idx]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N >> S;
+    for (int i = 0; i < N; i++) {
+        int num = 0;
+        cin >> num;
+        (i < N / 2 ? leftV : rightV).push_back(num);
+    }
+    solution(leftV, leftSum, 0, 0);
+    solution(rightV, rightSum, 0, 0);
+    sort(leftSum.begin(), leftSum.end());
+    for (const int d: rightSum) {
+        auto[lo, hi] = equal_range(leftSum.begin(), leftSum.end(), S - d);
+        answer += hi - lo;
+    }
+    cout << (S == 0 ? answer - 1 : answer) << "\n";
+    return 0;
+}
