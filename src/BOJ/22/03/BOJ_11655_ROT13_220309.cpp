c8afb9a99bdc9f87a2de5a24d67ecdcb7a9d16a0
Raemin Kang <foals18@gmail.com>
2022-03-09 08:28:47 +0900

    feat: 백준 11655번 ROT13 업로드

---
+++ src/BOJ/22/03/BOJ_11655_ROT13_220309.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<char> upperV;
+vector<char> lowerV;
+
+void init() {
+    for (int i = 'A'; i <= 'Z'; i++) upperV.push_back((char) i);
+    for (int i = 'a'; i <= 'z'; i++) lowerV.push_back((char) i);
+}
+
+char getChar(char c, vector<char> v) {
+    unsigned long currentPos = distance(v.begin(), find(v.begin(), v.end(), c));
+    unsigned long targetPos = (currentPos + 13) % v.size();
+    return v[targetPos];
+}
+
+char ROT13(char c) {
+    char result = c;
+    if (isalpha(c)) {
+        if (isupper(c)) {
+            result = getChar(c, upperV);
+        } else {
+            result = getChar(c, lowerV);
+        }
+    }
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    string str;
+    getline(cin, str);
+    init();
+    for (const char d: str) cout << ROT13(d);
+    cout << "\n";
+    return 0;
+}
