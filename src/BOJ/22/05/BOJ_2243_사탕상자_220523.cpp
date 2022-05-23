e7e0901347b8ad6b7270e996bfd573af7e7d5d4e
Raemerrr <foals18@gmail.com>
2022-05-24 06:49:51 +0900

    feat: 백준 2243번 사탕상자 업로드

---
+++ src/BOJ/22/05/BOJ_2243_사탕상자_220523.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+const int MAX = 1000001;
+int N;
+vector<int> segmentTree;
+
+void update_tree(int Node, int Start, int End, int Index, int Value) {
+    if (Index < Start || End < Index) return;
+    segmentTree[Node] += Value;
+    if (Start == End) return;
+    int Mid = (Start + End) / 2;
+    update_tree(Node * 2, Start, Mid, Index, Value);
+    update_tree(Node * 2 + 1, Mid + 1, End, Index, Value);
+}
+
+int query_tree(int Node, int Start, int End, int Cnt) {
+    if (Start == End) return Start;
+    int Mid = (Start + End) / 2;
+    if (Cnt <= segmentTree[Node * 2]) return query_tree(Node * 2, Start, Mid, Cnt);
+    return query_tree(Node * 2 + 1, Mid + 1, End, Cnt - segmentTree[Node * 2]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N;
+    int treeHeight = (int) ceil(log2(MAX));
+    int treeSize = (1 << (treeHeight + 1));
+    segmentTree.assign(treeSize, 0);
+    for (int i = 0; i < N; i++) {
+        int a = 0, b = 0, c = 0;
+        cin >> a >> b;
+        if (a == 1) {
+            int target = query_tree(1, 0, MAX - 1, b);
+            cout << target + 1 << "\n";
+            update_tree(1, 0, MAX - 1, target, -1);
+        } else {
+            cin >> c;
+            update_tree(1, 0, MAX - 1, b - 1, c);
+        }
+    }
+    return 0;
+}
