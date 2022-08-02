80be7d55acae67b31b58dc9eff5e234770d089d0
Raemerrr <foals18@gmail.com>
2022-08-03 08:28:09 +0900

    feat: 백준 17298번 오큰수 업로드

---
+++ src/BOJ/22/08/BOJ_17298_오큰수_220802.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> v, temp, answer;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N;
+    v.assign(N, 0);
+    answer.assign(N, -1);
+    for (int i = 0; i < N; i++) cin >> v[i];
+    for (int i = 0; i < N; i++) {
+        while (!temp.empty() && v[temp.back()] < v[i]) {
+            answer[temp.back()] = v[i];
+            temp.pop_back();
+        }
+        temp.push_back(i);
+    }
+    for (const int d: answer) cout << d << " ";
+    cout << "\n";
+    return 0;
+}
